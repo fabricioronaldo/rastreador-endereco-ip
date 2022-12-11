13d0fa8c4d586e97fe6d6ff15d2f53aefba1c2f3
Fabricio Velasco <fabriciorvelasco@gmail.com>
2022-12-10 22:12:19 -0300

    Desenvolvendo a responsividade no site.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/*API LeafletJS*/
 var map = L.map('map').setView([51.505, -0.09], 13);
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
@@ -9,3 +10,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
+/*API IP Geolocation API */
+
+    var ip = "138.117.54.4";
+    var api_key = "";
+    
+    $(function () {
+       $.ajax({
+           url: "https://geo.ipify.org/api/v1",
+           data: {apiKey: api_key, ipAddress: ip},
+           success: function(data) {
+               $("footer").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+           }
+       });
+    });
