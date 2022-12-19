9f86432ff7455eacdae8ff57552c0bad571c08fd
Fabricio Velasco <fabriciorvelasco@gmail.com>
2022-12-18 22:19:11 -0300

    Implementação da busca pelo formulário

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,10 +25,11 @@ marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
        });
     });*/
  
-    
+
     
     var api_key = "at_ycQeForGoXHTrty2ERE46JTJ2VmnP";
     
+
 
     $(function () {
         $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
@@ -65,5 +66,114 @@ marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 
 
+$("#button__search").click(function () {
+    
+    var search_ip = document.querySelector("#ip").value;
+    
 
+    
+    if($.isNumeric(search_ip)) {
+        
+            $.ajax({
+                url: "https://geo.ipify.org/api/v1",
+                data: {apiKey: api_key, ipAddress: search_ip},
+                success: function(data) {
+                
+                    a = JSON.stringify(data,"",2);
+                    b = JSON.parse(a);
+                    console.log(a);
+                    console.log(b);
+                    $("#ip_return").html(b.ip);
+                    $("#location_return").html(b.location.region);
+                    $("#timezone_return").html(b.location.timezone);
+                    $("#isp_return").html(b.isp);
+                    
+                    map = map.off()
+                    map = map.remove()
+        
+                    map = L.map('map').setView([b.location.lat, b.location.lng], 13);
+                    marker = L.marker([b.location.lat, b.location.lng]).addTo(map);
+                
+                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    }).addTo(map);
+                
+                    marker.bindPopup("<b>" + b.location.city + "</b><br>" + b.as.domain).openPopup();             
+        
+                }
+        
+            });
+        
+    } else {
+        $.ajax({
+            url: "https://geo.ipify.org/api/v1",
+            data: {apiKey: api_key, domain: search_ip},
+            success: function(data) {
+              
+                a = JSON.stringify(data,"",2);
+                b = JSON.parse(a);
+                console.log(a);
+                console.log(b);
+                $("#ip_return").html(b.ip);
+                $("#location_return").html(b.location.region);
+                $("#timezone_return").html(b.location.timezone);
+                $("#isp_return").html(b.isp);
+                
+                map = map.off()
+                map = map.remove()
+    
+                map = L.map('map').setView([b.location.lat, b.location.lng], 13);
+                marker = L.marker([b.location.lat, b.location.lng]).addTo(map);
+              
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+            
+                marker.bindPopup("<b>" + b.location.city + "</b><br>" + b.as.domain).openPopup();             
+    
+            }
+    
+        });
+    }
 
+});
+            /*$("#ip_return").val('');
+            $("#location_return").val('');
+            $("#timezone_return").append('');
+            $("#isp_return").append('');*/
+
+    /*$.ajax({
+        url: "https://geo.ipify.org/api/v1",
+        data: {apiKey: api_key, ipAddress: search_ip},
+        success: function(data) {
+          
+            a = JSON.stringify(data,"",2);
+            b = JSON.parse(a);
+            console.log(a);
+            console.log(b);
+            $("#ip_return").html(b.ip);
+            $("#location_return").html(b.location.region);
+            $("#timezone_return").html(b.location.timezone);
+            $("#isp_return").html(b.isp);
+            
+            map = map.off()
+            map = map.remove()
+
+            map = L.map('map').setView([b.location.lat, b.location.lng], 13);
+            marker = L.marker([b.location.lat, b.location.lng]).addTo(map);
+          
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        
+            marker.bindPopup("<b>" + b.location.city + "</b><br>" + b.as.domain).openPopup();             
+
+        }
+
+    });
+});
+
+*/
